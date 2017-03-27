450597b34eecc7625767b65260fb46ffacfb36ad
stropitek <kostro.d@gmail.com>
2017-03-28 00:00:24 +0200

    fix load libsvm.wasm in production build

--- src/wasmPreJS.js
+++ src/wasmPreJS.js
@@ -1,8 +1,12 @@
 const path = require('path');
 var Module = module.exports = {};
 
-// When loaded with nodejs, the absolute file path is neede
-Module.wasmBinaryFile = path.resolve(__dirname, 'libsvm.wasm');
+if(typeof window === 'undefined') {
+// When loaded with nodejs, the absolute file path is needed
+    Module.wasmBinaryFile = path.resolve(__dirname, 'libsvm.wasm');
+} else {
+    Module.wasmBinaryFile = 'libsvm.wasm';
+}
 
 Module.isReady = new Promise(function (resolve) {
     Module.onRuntimeInitialized = function () {
