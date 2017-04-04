d718735a5af5fa490468ba7ce5f14d670c9121fc
stropitek <kostro.d@gmail.com>
2017-04-04 21:05:38 +0200

    demo: Update Home

--- demos/components/Home.js
+++ demos/components/Home.js
@@ -6,7 +6,7 @@ export default function () {
         <div>
             <h1>LibSVM-js</h1>
             <p><Link to="/SVC">Support vector classification</Link></p>
-
+            <p><Link to="/SVR">Support vector regression</Link></p>
         </div>
     );
 }
